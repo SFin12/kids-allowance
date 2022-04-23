4f740c01d2389ca7f358cd1ff7022f02e93d0503
Sean Finegan <sean.j.finegan@gmail.com>
2022-04-23 13:55:18 -0700

    sorts chores alphabetically when being displayed

--- src/Pages/ChoresPage/ChoresPage.js
+++ src/Pages/ChoresPage/ChoresPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import ChoreCard from "../../components/Chores/ChoreCard";
@@ -6,28 +6,37 @@ import { selectChores } from "../../features/chores/choresSlice";
 import { getChores } from "../../utils/firestore";
 
 export default function ChoresPage() {
+    const [sortedChores, setSortedChores] = useState([]);
     const chores = useSelector(selectChores);
 
     useEffect(() => {
-        console.log("chores: ", chores);
+        // Creates sorted array of keys to look up chore info and display alphabetically
+        setSortedChores(
+            Object.keys(chores)
+                .map((chore, i) => chore)
+                .sort()
+        );
     }, [chores]);
 
     return (
         <div className="w-100">
             <Container className="d-flex justify-content-center pt-3">
                 <Row xs={1} md={2} lg={3} xl={3} xxl={4} className={"w-100"}>
-                    {Object.keys(chores).map((key, i) => (
-                        <Col
-                            key={i + key}
-                            className="d-flex justify-content-center"
-                        >
-                            <ChoreCard
-                                chore={key}
-                                value={chores[key].value}
-                                completedBy={chores[key].completedBy}
-                            />
-                        </Col>
-                    ))}
+                    {/* Creates a card for each chore with value used for front and back of card */}
+                    {sortedChores.map((key, i) => {
+                        return (
+                            <Col
+                                key={i + key}
+                                className="d-flex justify-content-center"
+                            >
+                                <ChoreCard
+                                    chore={key}
+                                    value={chores[key].value}
+                                    completedBy={chores[key].completedBy}
+                                />
+                            </Col>
+                        );
+                    })}
                 </Row>
             </Container>
         </div>
