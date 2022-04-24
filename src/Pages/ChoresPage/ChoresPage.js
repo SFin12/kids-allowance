40f013f005a6ae0ed4555577d622aec790dc9436
Sean Finegan <sean.j.finegan@gmail.com>
2022-04-24 13:47:20 -0700

    Added no chore message if there are no chores

--- src/Pages/ChoresPage/ChoresPage.js
+++ src/Pages/ChoresPage/ChoresPage.js
@@ -8,7 +8,7 @@ import { getChores } from "../../utils/firestore";
 export default function ChoresPage() {
     const [sortedChores, setSortedChores] = useState([]);
     const chores = useSelector(selectChores);
-
+    console.log(chores);
     useEffect(() => {
         // Creates sorted array of keys to look up chore info and display alphabetically
         setSortedChores(
@@ -19,26 +19,46 @@ export default function ChoresPage() {
     }, [chores]);
 
     return (
-        <div className="w-100">
-            <Container className="d-flex justify-content-center pt-3">
-                <Row xs={1} md={2} lg={3} xl={3} xxl={4} className={"w-100"}>
-                    {/* Creates a card for each chore with value used for front and back of card */}
-                    {sortedChores.map((key, i) => {
-                        return (
-                            <Col
-                                key={i + key}
-                                className="d-flex justify-content-center"
-                            >
-                                <ChoreCard
-                                    chore={key}
-                                    value={chores[key].value}
-                                    completedBy={chores[key].completedBy}
-                                />
-                            </Col>
-                        );
-                    })}
-                </Row>
-            </Container>
-        </div>
+        <>
+            {sortedChores.length ? (
+                <div className="w-100">
+                    <Container className="d-flex justify-content-center pt-3">
+                        <Row
+                            xs={1}
+                            md={2}
+                            lg={3}
+                            xl={3}
+                            xxl={4}
+                            className={"w-100"}
+                        >
+                            {/* Creates a card for each chore with value used for front and back of card */}
+                            {sortedChores.map((key, i) => {
+                                return (
+                                    <Col
+                                        key={i + key}
+                                        className="d-flex justify-content-center"
+                                    >
+                                        <ChoreCard
+                                            chore={key}
+                                            value={chores[key].value}
+                                            completedBy={
+                                                chores[key].completedBy
+                                            }
+                                        />
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    </Container>
+                </div>
+            ) : (
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <h4 className="w-75">
+                        You don't have any chores. Go to settings to create
+                        chores.
+                    </h4>
+                </div>
+            )}
+        </>
     );
 }
