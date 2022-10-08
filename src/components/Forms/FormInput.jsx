2d1d525ad5b4f6924d3cc37bf7b183023a414e65
Sean Finegan <sean.j.finegan@gmail.com>
2022-10-08 13:53:22 -0700

    Updated app icons and fixed adjust total increments.

--- src/components/Forms/FormInput.jsx
+++ src/components/Forms/FormInput.jsx
@@ -17,55 +17,37 @@ export default function FormInput({
     const pointsType = useSelector(selectPointsType)
 
     return (
-        <Form onSubmit={handleSubmit} id="form">
-            <FormGroup>
-                {titles.map((title, i) => (
-                    <div key={i + title} style={{ marginBottom: 20 }}>
-                        <FormLabel>{title}</FormLabel>
-                        <div className="d-flex align-items-center">
-                            {title === "Cost" ||
-                            title === "Value" ||
-                            title === "Amount" ? (
-                                <span
-                                    style={{
-                                        fontSize: "1.2rem",
-                                        paddingRight: 3.91,
-                                    }}
-                                >
-                                    {pointsType.icon}
-                                </span>
-                            ) : (
-                                <span
-                                    style={{
-                                        fontSize: "1.2rem",
-                                        paddingRight: 3.91,
-                                    }}
-                                >
-                                    &nbsp;&nbsp;
-                                </span>
-                            )}
-                            <FormControl
-                                name={title}
-                                type={
-                                    title === "Cost" ||
-                                    title === "Value" ||
-                                    title === "Amount"
-                                        ? "number"
-                                        : "text"
-                                }
-                                min={0}
-                                presicion={2}
-                                step={0.25}
-                                defaultValue=""
-                                placeholder={placeholders[i]}
-                                required
-                                maxLength={20}
-                            ></FormControl>
-                        </div>
-                    </div>
-                ))}
-                <Button type="submit">Save</Button>
-            </FormGroup>
-        </Form>
-    );
+      <Form onSubmit={handleSubmit} id="form">
+        <FormGroup>
+          {titles.map((title, i) => (
+            <div key={i + title} style={{ marginBottom: 20 }}>
+              <FormLabel>{title}</FormLabel>
+              <div className="d-flex align-items-center">
+                {title === "Cost" || title === "Value" || title === "Amount" ? (
+                  <span
+                    style={{
+                      fontSize: "1.2rem",
+                      paddingRight: 3.91,
+                    }}
+                  >
+                    {pointsType.icon}
+                  </span>
+                ) : (
+                  <span
+                    style={{
+                      fontSize: "1.2rem",
+                      paddingRight: 3.91,
+                    }}
+                  >
+                    &nbsp;&nbsp;
+                  </span>
+                )}
+                <FormControl name={title} type={title === "Cost" || title === "Value" || title === "Amount" ? "number" : "text"} min={0} presicion={2} step={pointsType.type === "money" ? 0.25 : 1} defaultValue="" placeholder={placeholders[i]} required maxLength={20}></FormControl>
+              </div>
+            </div>
+          ))}
+          <Button type="submit">Save</Button>
+        </FormGroup>
+      </Form>
+    )
 }
